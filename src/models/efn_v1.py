0af3798bcd1730a57b20cb04a7ea6fe5cf38ef5e
komekami <komekami0131@gmail.com>
2024-04-02 07:35:51 +0900

    revert

--- src/models/efn_v1.py
+++ src/models/efn_v1.py
@@ -2,8 +2,6 @@ import tensorflow as tf
 
 import efficientnet.tfkeras as efn
 
-from src.losses.arcface_loss import ArcFaceLoss
-
 
 def build_model():
     input = tf.keras.Input(shape=(128, 256, 8))
@@ -37,18 +35,13 @@ def build_model():
     x = base_model(x)
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
     # x = tf.keras.layers.Dropout(0.25)(x)
-
-    # x = tf.keras.layers.Dense(6, activation="softmax", dtype="float32")(x)
-    arcface_layer = ArcFaceLayer0(num_outputs=6, kernel_regularizer=tf.keras.regularizers.l2(1e-4))
-    x = arcface_layer(x)
+    x = tf.keras.layers.Dense(6, activation="softmax", dtype="float32")(x)
 
     # compile
     model = tf.keras.Model(inputs=input, outputs=x)
     opt = tf.keras.optimizers.Adam(learning_rate=1e-3)
     # https://www.tensorflow.org/api_docs/python/tf/keras/losses/KLDivergence
-    # loss = tf.keras.losses.KLDivergence()
-    # loss = ArcFaceLoss(loss_func=tf.keras.losses.KLDivergence())
-    loss = ArcFaceLoss(loss_func=tf.keras.losses.CategoricalCrossentropy(from_logits=True))
+    loss = tf.keras.losses.KLDivergence()
 
     model.compile(loss=loss, optimizer=opt)
 
@@ -69,29 +62,3 @@ def unfreeze_model(model):
             layer.trainable = False
 
     return model
-
-
-class ArcFaceLayer0(tf.keras.layers.Layer):
-    def __init__(self, num_outputs, kernel_regularizer=None):
-        super().__init__()
-
-        self.num_outputs = num_outputs
-        self.kernel_regularizer = kernel_regularizer
-
-    def build(self, input_shape):
-        weight_shape = (input_shape[-1], self.num_outputs)
-        self.kernel = self.add_weight(
-            name="kernel",
-            shape=weight_shape,
-            initializer=tf.keras.initializers.TruncatedNormal(),
-            regularizer=self.kernel_regularizer,
-            trainable=True,
-        )
-
-        super().build(input_shape)
-
-    def call(self, input):
-        n_inputs = tf.math.l2_normalize(input, axis=1)
-        n_kernels = tf.math.l2_normalize(self.kernel, axis=0)
-
-        return tf.matmul(n_inputs, n_kernels)  # W.Txの内積
